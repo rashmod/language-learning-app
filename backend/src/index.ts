b03f3087d7639ef09cadbade8916dd55e61e430c
Bhushan Alhat <alhatbhushan@gmail.com>
2023-09-25 22:08:57 +0530

    [Backend] added cors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
 
 import connectDB from './db/prisma';
 import UserRoutes from './routes/UserRoutes';
@@ -14,9 +15,10 @@ dotenv.config();
 
 const app = express();
 
-connectDB();
-
 app.use(express.json());
+app.use(cors());
+
+connectDB();
 
 /*
 todo 
