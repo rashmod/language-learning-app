f83361acc9d4133354191bc7697ac4ee592fb1d4
Bhushan Alhat <alhatbhushan@gmail.com>
2023-09-25 21:21:24 +0530

    [Frontend] added react router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,19 @@
+import { Routes, Route } from 'react-router-dom';
+
+import Home from './pages/Home';
+import Navbar from './components/Navbar';
+
 function App() {
-	return <h1 className='bg-red-300'>hello world</h1>;
+	return (
+		<h1 className='flex flex-col items-center min-h-screen'>
+			<div className='w-10/12 grow'>
+				<Navbar />
+				<Routes>
+					<Route path='/' element={<Home />} />
+				</Routes>
+			</div>
+		</h1>
+	);
 }
 
 export default App;
