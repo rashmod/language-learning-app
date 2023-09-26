eddb85a75381642e752e24f3c8fb0cbb957f3f1b
Bhushan Alhat <alhatbhushan@gmail.com>
2023-09-26 18:37:29 +0530

    [Backend] setup express session

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
+import expressSession from 'express-session';
 
 import connectDB from './db/prisma';
 import UserRoutes from './routes/UserRoutes';
@@ -11,9 +12,46 @@ import OptionRoutes from './routes/OptionRoutes';
 import TestResultRoutes from './routes/TestResultRoutes';
 import errorHandler from './middlewares/errorHandler';
 
+declare module 'express-session' {
+	interface SessionData {
+		user: any;
+	}
+}
+
 dotenv.config();
 
 const app = express();
+
+if (!process.env.COOKIE_SECRET) {
+	throw new Error('Set cookie secret in the environment variable');
+}
+
+if (process.env.NODE_ENV === 'PRODUCTION') {
+	app.set('trust proxy', 1);
+
+	app.use(
+		expressSession({
+			secret: process.env.COOKIE_SECRET,
+			resave: true,
+			saveUninitialized: true,
+			cookie: {
+				sameSite: 'none',
+				secure: true,
+				maxAge: 1000 * 60 * 60 * 24 * 7, // one week
+			},
+		})
+	);
+}
+
+if (process.env.NODE_ENV === 'DEVELOPMENT') {
+	app.use(
+		expressSession({
+			secret: process.env.COOKIE_SECRET,
+			resave: true,
+			saveUninitialized: true,
+		})
+	);
+}
 
 app.use(express.json());
 app.use(cors());
