a4c3ab3de3ed099497e5c33d524c88c68b67653b
Bhushan Alhat <alhatbhushan@gmail.com>
2023-09-29 00:00:28 +0530

    [Frontend] implemented api function for adding user language

--- frontend/src/api/languages.ts
+++ frontend/src/api/languages.ts
@@ -13,13 +13,22 @@ export const getAllLanguages = async (): Promise<{
 	return data;
 };
 
-// export const getUserLanguages = async (userId: string) => {
-// 	const res = await axios.get(
-// 		`${import.meta.env.VITE_BACKEND_BASE_URL}/api/listings/${userId}`
-// 	);
-// 	const { data } = await res.data;
-// 	return data;
-// };
+export const getUserLanguages = async (
+	userId: string
+): Promise<{ success: boolean; count: number; data: TUserLanguage[] }> => {
+	const res = await axios.get(
+		`${import.meta.env.VITE_BACKEND_BASE_URL}/api/users/${userId}/languages`
+	);
+	const data = await res.data;
+	return data;
+};
+
+export type TUserLanguage = {
+	proficiencyLevel: number;
+	score: number;
+	languageId: string;
+	userId: string;
+};
 
 export type TLanguage = {
 	languageId: string;
