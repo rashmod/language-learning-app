7ab37b5a9165e267d46c6d4e71fcdcf4319f1943
Bhushan Alhat <alhatbhushan@gmail.com>
2023-09-28 19:57:43 +0530

    [Backend] added infinite questions controller and route

--- backend/src/controllers/Testcontrollers.ts
+++ backend/src/controllers/Testcontrollers.ts
@@ -2,6 +2,7 @@ import { Request, Response } from 'express';
 
 import { prisma } from '../db/prisma';
 import { DuplicateResourceError, NotFoundError } from '../utilities/Errors';
+import getRandomElements from '../seed/utilities/getRandomElements';
 
 // @desc Get all tests of a language
 // @route GET /api/languages/:languageId/tests
@@ -77,4 +78,101 @@ export const createTest = async (req: Request, res: Response) => {
 	});
 
 	res.status(200).json({ success: true, data: test });
+};
+
+// @desc Get infinite questions of language
+// @route POST /api/languages/:languageId/tests/infinite
+// @access user
+export const getInfiniteQuestions = async (req: Request, res: Response) => {
+	const { languageId } = req.params;
+	const { scoreAchieved, maxScorePossible, questionBatchSize } = req.query;
+
+	const numberScoreAchieved = Number(scoreAchieved);
+	const numberMaxScorePossible = Number(maxScorePossible);
+	const numberQuestionBatchSize = Number(questionBatchSize);
+
+	const language = await prisma.language.findUnique({
+		where: { languageId },
+	});
+
+	if (!language)
+		throw new NotFoundError('The requested language was not found');
+
+	const percentage = Number(
+		((numberScoreAchieved / numberMaxScorePossible) * 100).toFixed(0)
+	);
+	const averageScore = Number(
+		(numberMaxScorePossible / numberQuestionBatchSize).toFixed(0)
+	);
+	const easierQuestions = {
+		difficulty: 0,
+		params: { lte: averageScore },
+	} as const;
+	const similarQuestions = {
+		difficulty: 1,
+		params: { lte: averageScore + 1, gte: averageScore - 1 },
+	} as const;
+	const harderQuestions = {
+		difficulty: 2,
+		params: { gte: averageScore },
+	} as const;
+	const difficulty: {
+		difficulty: number;
+		params: { lte?: number; gte?: number };
+	} =
+		percentage < 50
+			? easierQuestions
+			: percentage < 75
+			? similarQuestions
+			: harderQuestions;
+
+	const questions = await prisma.question.findMany({
+		where: { test: { languageId }, difficulty: difficulty.params },
+		include: { options: true },
+		take: numberQuestionBatchSize * 5,
+	});
+
+	questions.splice(
+		0,
+		questions.length,
+		...getRandomElements(questions, numberQuestionBatchSize)
+	);
+
+	if (questions.length < numberQuestionBatchSize) {
+		const difference = numberQuestionBatchSize - questions.length;
+		const res = await prisma.question.findMany({
+			where: {
+				test: { languageId },
+				difficulty: { lte: difficulty.params.gte },
+			},
+			include: { options: true },
+			take: difference * 5,
+		});
+
+		res.splice(0, res.length, ...getRandomElements(res, difference));
+
+		questions.push(...res);
+	}
+
+	if (questions.length < numberQuestionBatchSize) {
+		const difference = numberQuestionBatchSize - questions.length;
+		const res = await prisma.question.findMany({
+			where: {
+				test: { languageId },
+				difficulty: { gte: difficulty.params.lte },
+			},
+			include: { options: true },
+			take: difference * 5,
+		});
+
+		res.splice(0, res.length, ...getRandomElements(res, difference));
+
+		questions.push(...res);
+	}
+
+	res.status(200).json({
+		success: true,
+		count: questions.length,
+		data: questions,
+	});
 };
--- backend/src/routes/TestRoutes.ts
+++ backend/src/routes/TestRoutes.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import {
 	createTest,
 	getAllTests,
+	getInfiniteQuestions,
 	getTest,
 } from '../controllers/TestControllers';
 import catchAsyncError from '../utilities/catchAsyncError';
@@ -13,6 +14,7 @@ const router = Router({ mergeParams: true });
 router.get('/', isSignedIn, catchAsyncError(getAllTests));
 router.post('/', isSignedIn, catchAsyncError(createTest));
 
+router.get('/infinite', isSignedIn, catchAsyncError(getInfiniteQuestions));
 router.get('/:testId', isSignedIn, catchAsyncError(getTest));
 
 export default router;
